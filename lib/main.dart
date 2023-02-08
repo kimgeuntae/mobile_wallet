08156841cd832117279e988b885a050a8b760db6
geuntae.kim <kok617@naver.com>
2023-02-08 20:30:04 +0900

    Add: padding

--- lib/main.dart
+++ lib/main.dart
@@ -14,40 +14,42 @@ class MyApp extends StatelessWidget {
       title: 'Wallet',
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
-        body:
-            // Column : 세로
-            Column(
-          children: [
-            const SizedBox(
-              height: 80,
-            ),
-            // Row : 가로
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Hey, Selena',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          // Column : 세로
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              // Row : 가로
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
-                        fontSize: 18,
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              ],
-            )
-          ],
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
